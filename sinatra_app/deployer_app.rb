d9d6f856b136a07041fed82be9140e178ca96963
Jon Moses <jon@burningbush.us>
2012-09-07 13:50:54 -0400

    Changes

--- sinatra_app/deployer_app.rb
+++ sinatra_app/deployer_app.rb
@@ -20,7 +20,7 @@ class DeployerApp < Sinatra::Base
     end
 
     def bundle(repo_name)
-      do_in(repo_name, "bundle")
+      do_in_as_login(repo_name, "bundle --no-color")
     end
 
     def deploy(repo_name, environment, task = "deploy")
@@ -32,7 +32,7 @@ class DeployerApp < Sinatra::Base
         return "Failed bundling: #{result}"
       end
 
-      unless success?(result = do_in(repo_name, "bundle exec cap #{environment} #{task}"))
+      unless success?(result = do_in_as_login(repo_name, "bin/cap #{environment} #{task}"))
         return "Failed deploying: #{result}"
       end
 
@@ -43,8 +43,22 @@ class DeployerApp < Sinatra::Base
       `cd #{File.join BASE, repo_name} && #{command} 2>&1`
     end
 
+    def do_in_as_login(repo_name, command)
+      user = `whoami`
+
+      `sudo -u #{user} -i #{command}`
+    end
+
     def success?(response)
       $?.exitstatus.zero?
+    end
+
+    def env_vars(repo)
+      {
+        'BUNDLE_GEMFILE' => File.join(BASE, repo, 'Gemfile'),
+        'GEM_HOME' => File.join(BASE, repo, 'vendor/bundle/ruby/1.9.1'),
+        'GEM_PATH' => '',
+      }.inject("") {|memo, (key, value)| "#{key}=#{value} #{memo}" }
     end
 
 
