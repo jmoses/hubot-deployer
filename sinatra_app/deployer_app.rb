8412111b15ea7c9fc8c54f476f8f97136e2ed281
Jon Moses <jon@burningbush.us>
2012-09-07 14:07:56 -0400

    No rackup

--- sinatra_app/deployer_app.rb
+++ sinatra_app/deployer_app.rb
@@ -10,53 +10,51 @@ class NilClass
   end
 end
 
-class DeployerApp < Sinatra::Base
-  module RepoUpdater
-    BASE = File.read('.basedir').strip
+module RepoUpdater
+  BASE = File.read('.basedir').strip
 
-    module_function
-    def update(repo_name)
-      do_in(repo_name, "git pull origin master")
-    end
-
-    def bundle(repo_name)
-      do_in_as_login(repo_name, "bundle --no-color")
-    end
-
-    def deploy(repo_name, environment, task = "deploy")
-      unless success?(result = update(repo_name))
-        return "Failed updating the repo: #{result}"
-      end
-
-      unless success?(result = bundle(repo_name))
-        return "Failed bundling: #{result}"
-      end
-
-      unless success?(result = do_in(repo_name, "bin/cap #{environment} #{task}"))
-        return "Failed deploying: #{result}"
-      end
-
-      return "Deploy complete."
-    end
-
-    def do_in(repo_name, command)
-      `cd #{File.join BASE, repo_name} && #{command} 2>&1`
-    end
-
-    def success?(response)
-      $?.exitstatus.zero?
-    end
-
+  module_function
+  def update(repo_name)
+    do_in(repo_name, "git pull origin master")
   end
 
-  #pass in the repo name and deploy that shit
-  get '/deploy/:name/:environment/?:command' do
-    result = RepoUpdater.deploy(params[:name], params[:environment], params[:command].blank? ? params[:command] : 'deploy')
+  def bundle(repo_name)
+    do_in_as_login(repo_name, "bundle --no-color")
+  end
 
-    if result != "Deploy complete."
-      [500, result]
-    else
-      result
+  def deploy(repo_name, environment, task = "deploy")
+    unless success?(result = update(repo_name))
+      return "Failed updating the repo: #{result}"
     end
+
+    unless success?(result = bundle(repo_name))
+      return "Failed bundling: #{result}"
+    end
+
+    unless success?(result = do_in(repo_name, "bin/cap #{environment} #{task}"))
+      return "Failed deploying: #{result}"
+    end
+
+    return "Deploy complete."
+  end
+
+  def do_in(repo_name, command)
+    `cd #{File.join BASE, repo_name} && #{command} 2>&1`
+  end
+
+  def success?(response)
+    $?.exitstatus.zero?
+  end
+
+end
+
+#pass in the repo name and deploy that shit
+get '/deploy/:name/:environment/?:command' do
+  result = RepoUpdater.deploy(params[:name], params[:environment], params[:command].blank? ? params[:command] : 'deploy')
+
+  if result != "Deploy complete."
+    [500, result]
+  else
+    result
   end
 end
